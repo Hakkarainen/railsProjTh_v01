8ab46b032da13fc83d95d1a24f11f4bb9edf14d4
Hakkarainen <thyyppa@gmail.com>
2016-03-19 17:07:52 +0200

    2nd commit2016-03-19

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_railsProjTh_v01_session'
+Rails.application.config.session_store :cookie_store, key: '_untitled_session'
